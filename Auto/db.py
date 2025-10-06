0d10bb613beb3aa09cd3d35418639a5db65de848
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-07 04:49:08 +0530

    Create Auto/db.py

---
+++ Auto/db.py
@@ -0,0 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MONGO_URL = "YOUR_MONGO_URL"
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo["AutoApprovalBot"]
+
+users = db["users"]
+approvals = db["approvals"]
+
+async def add_user(user_id: int):
+    await users.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
+
+async def get_all_users():
+    return [u["_id"] async for u in users.find({})]
+
+async def set_approval(chat_id: int, status: bool):
+    await approvals.update_one({"_id": chat_id}, {"$set": {"status": status}}, upsert=True)
+
+async def get_approval(chat_id: int):
+    data = await approvals.find_one({"_id": chat_id})
+    return data["status"] if data else False
