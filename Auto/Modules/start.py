7aa3439c39a26764c3bf5aa0bd9cff9e7d9e926f
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-22 14:18:18 +0530

    Update start.py

--- Auto/Modules/start.py
+++ Auto/Modules/start.py
@@ -6,21 +6,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     await add_user(user.id)
 
-    keyboard = [
-        [InlineKeyboardButton("➕ ADD ME TO YOUR CHANNEL", url="https://t.me/AlphaXRequestBot?startchannel=true")],
-        [InlineKeyboardButton("➕ ADD ME TO YOUR GROUP", url="https://t.me/AlphaXRequestBot")]
+    keyboard = [  
+        [InlineKeyboardButton("➕ ADD ME TO YOUR CHANNEL", url="https://t.me/AlphaXRequestBot?startchannel=true")],  
+        [InlineKeyboardButton("➕ ADD ME TO YOUR GROUP", url="https://t.me/AlphaXRequestBot")]  
+    ]
 
-    await update.message.reply_photo(
-        "https://files.catbox.moe/1vkwk6.jpg",
-        caption=(
-            f"Hey {user.mention_html()} 💖,\n\n"
-            "I'm <b>Auto Request Approver Bot 🤖!</b>\n"
-            "I can approve new join requests in chats.\n"
-            "Just add me to the chat with <b>invite users permission</b>.\n\n"
-            "Use the button below to add me 👇"
-        ),
-        parse_mode="HTML",
-        reply_markup=InlineKeyboardMarkup(keyboard)
+    await update.message.reply_photo(  
+        "https://files.catbox.moe/1vkwk6.jpg",  
+        caption=(  
+            f"Hey {user.mention_html()} 💖,\n\n"  
+            "I'm <b>Auto Request Approver Bot 🤖!</b>\n"  
+            "I can approve new join requests in chats.\n"  
+            "Just add me to the chat with <b>invite users permission</b>.\n\n"  
+            "Use the button below to add me 👇"  
+        ),  
+        parse_mode="HTML",  
+        reply_markup=InlineKeyboardMarkup(keyboard)  
     )
 
 def register(application):
